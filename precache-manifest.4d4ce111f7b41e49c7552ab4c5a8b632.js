8a8f4dccc7377bc42da503b99604f88f5b56de40
Tony Pham <tony.pham@pch.gc.ca>
2021-01-25 17:53:30 -0500

    Updates

---
+++ precache-manifest.4d4ce111f7b41e49c7552ab4c5a8b632.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086fbcb98b05ee1370f0b71bdc45b27e",
+    "url": "/ecu-sample/index.html"
+  },
+  {
+    "revision": "7138232535f9cbdacf88",
+    "url": "/ecu-sample/static/css/main.8a5ca94f.chunk.css"
+  },
+  {
+    "revision": "273f68244129b15bdd45",
+    "url": "/ecu-sample/static/js/2.e8b8b5e7.chunk.js"
+  },
+  {
+    "revision": "4c98c6b556b8cc39dae85626e0284631",
+    "url": "/ecu-sample/static/js/2.e8b8b5e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7138232535f9cbdacf88",
+    "url": "/ecu-sample/static/js/main.896d3c6a.chunk.js"
+  },
+  {
+    "revision": "2c70d2d007f31d7ddf4e",
+    "url": "/ecu-sample/static/js/runtime-main.ffd9ae49.js"
+  },
+  {
+    "revision": "0c9f225e8f69c622f681cf1ed973cc3d",
+    "url": "/ecu-sample/static/media/fa-brands-400.0c9f225e.woff2"
+  },
+  {
+    "revision": "0ce1e868452204695c8ac1c70f693c2d",
+    "url": "/ecu-sample/static/media/fa-brands-400.0ce1e868.woff"
+  },
+  {
+    "revision": "5d10d00980077f67990f73b1961bc46e",
+    "url": "/ecu-sample/static/media/fa-brands-400.5d10d009.svg"
+  },
+  {
+    "revision": "d28c96cabe5302f1b2c90275cd3f4e69",
+    "url": "/ecu-sample/static/media/fa-brands-400.d28c96ca.eot"
+  },
+  {
+    "revision": "dfe5aa4344a2d8a29aec8d83fb3fb14e",
+    "url": "/ecu-sample/static/media/fa-brands-400.dfe5aa43.ttf"
+  },
+  {
+    "revision": "0232fe8b09de812c88e6cc27f57a3a88",
+    "url": "/ecu-sample/static/media/fa-regular-400.0232fe8b.eot"
+  },
+  {
+    "revision": "398be22b2308585b84da8e858e2d7b60",
+    "url": "/ecu-sample/static/media/fa-regular-400.398be22b.ttf"
+  },
+  {
+    "revision": "3cace4a04d941b5981ba32c6ce9afae1",
+    "url": "/ecu-sample/static/media/fa-regular-400.3cace4a0.woff"
+  },
+  {
+    "revision": "847712aaabbeba674afdda86d31cab17",
+    "url": "/ecu-sample/static/media/fa-regular-400.847712aa.woff2"
+  },
+  {
+    "revision": "d0fdc7c20b0b0b1aaf27327547735fa3",
+    "url": "/ecu-sample/static/media/fa-regular-400.d0fdc7c2.svg"
+  },
+  {
+    "revision": "00bb62bc33034479d5e82a0fd1c5efe3",
+    "url": "/ecu-sample/static/media/fa-solid-900.00bb62bc.eot"
+  },
+  {
+    "revision": "2306f642b31b774ba1d8b6b784539e19",
+    "url": "/ecu-sample/static/media/fa-solid-900.2306f642.svg"
+  },
+  {
+    "revision": "260d033f79f75d670fd09d8cf7c8bf8d",
+    "url": "/ecu-sample/static/media/fa-solid-900.260d033f.ttf"
+  },
+  {
+    "revision": "4bfbf7eb4b19d9ff9293eb177b6d0070",
+    "url": "/ecu-sample/static/media/fa-solid-900.4bfbf7eb.woff"
+  },
+  {
+    "revision": "9ae050d1876ac1763eb6afe4264e6d5a",
+    "url": "/ecu-sample/static/media/fa-solid-900.9ae050d1.woff2"
+  },
+  {
+    "revision": "e4ce8eec6dbfaeff01a0088996d15a97",
+    "url": "/ecu-sample/static/media/krakenimages.e4ce8eec.jpg"
+  }
+]);
